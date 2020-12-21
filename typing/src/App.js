70192a041b69120415613e11c95e1c58407d5c9b
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-12-21 16:12:23 -0500

    adding input element for user

--- typing/src/App.js
+++ typing/src/App.js
@@ -1,10 +1,19 @@
 import './App.scss';
 import Example from './components/example';
+import Input from './components/input';
 
 function App() {
+
+  function handleKeyPress(e) {
+    console.log('pressed');
+    // if (e.key === "h") {
+    //   console.log('correct');
+    // }
+  }
   return (
-    <div className="App">
+    <div className="App" onKeyPress={handleKeyPress}>
       <Example />
+      <Input />
     </div>
   );
 }
--- typing/src/components/example.js
+++ typing/src/components/example.js
@@ -3,12 +3,18 @@ import React from 'react';
 
 function Example() {
 
-  let input = 'hey there';
+  
+
+  let input = 'hey there how are you doing today i am doing quite well';
 
   const sentence = input.split("");
+  let curr = 0;
+
 
   return (
-    <div>{sentence[0]}</div>
+    <div >
+      {sentence.slice(curr, curr + 15)}
+    </div>
   );
 }
 
