b0b9d96fd19fce3c7b5cd537637f24286fa429ef
martin-stefan <55285748+martin-stefan@users.noreply.github.com>
2020-12-21 17:01:31 -0500

    adding main file

--- typing/src/App.js
+++ typing/src/App.js
@@ -1,19 +1,18 @@
 import './App.scss';
-import Example from './components/example';
-import Input from './components/input';
+
+import Main from './containers/main';
 
 function App() {
 
   function handleKeyPress(e) {
     console.log('pressed');
-    // if (e.key === "h") {
-    //   console.log('correct');
-    // }
+    if (e.key === "h") {
+      console.log('correct');
+    }
   }
   return (
     <div className="App" onKeyPress={handleKeyPress}>
-      <Example />
-      <Input />
+      <Main />
     </div>
   );
 }
